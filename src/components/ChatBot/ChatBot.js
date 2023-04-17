dbe55ab485bc436f0b8a532daef8f15509ee2218
aliibnemasud <mdmasudrony@gmail.com>
2023-04-17 15:48:54 +0600

    error commited

--- src/components/ChatBot/ChatBot.js
+++ src/components/ChatBot/ChatBot.js
@@ -1,44 +1,67 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./ChaBot.css";
+import axios from "axios";
 
 const ChatBot = () => {
+  const parentRef = useRef(null);
+  const inputMsgRef = useRef("");
+  const [error, setError] = useState('')
 
-    const parentRef = useRef(null);
-
-  const handleClick = () => {
+  const handleClick = async () => {
     const box = document.createElement("div");
-    box.classList.add("item", "right");  
+    box.classList.add("item", "right");
     const boxMessage = document.createElement("div");
     boxMessage.classList.add("msg");
-    boxMessage.innerHTML += `<p>This is message</p>`;
-    box.appendChild(boxMessage);    
+    boxMessage.innerHTML += `<p>${inputMsgRef.current.value}</p>`;
+    box.appendChild(boxMessage);
     boxMessage.classList.add("msg");
     parentRef.current.appendChild(box);
 
-    /// Response  ///    
-    
-    const item = document.createElement("div");
-    item.classList.add("item");
-    const icon = document.createElement("div");
-    icon.classList.add('icon')
-    const iconSvf = document.createElement("i");
-    iconSvf.classList.add('fa', 'fa-user')
-    const replay = document.createElement("div");
-    replay.classList.add("msg");
-    replay.innerHTML = `<p>Hello everyone, How are you?</p>`;
+    /// Response  ///
 
-    item.appendChild(icon)
-    icon.appendChild(iconSvf)
-    item.appendChild(replay)
-    parentRef.current.appendChild(item);
+    try {
+      await axios
+        .post(
+          "https://e63f4y64m4.execute-api.ap-south-1.amazonaws.com/prod/process_input",
+          {
+            question: inputMsgRef.current.value,
+          }
+        )
+        .then((res) => {
+          try {
+            const response = res.data.processed_output;
 
+            const item = document.createElement("div");
+            item.classList.add("item");
+            const icon = document.createElement("div");
+            icon.classList.add("icon");
+            const iconSvf = document.createElement("i");
+            iconSvf.classList.add("fa", "fa-user");
+            const replay = document.createElement("div");
+            replay.classList.add("msg");
+            replay.innerHTML = `<p>${response}</p>`;
+
+            item.appendChild(icon);
+            icon.appendChild(iconSvf);
+            item.appendChild(replay);
+            parentRef.current.appendChild(item);
+
+          } catch (error) {
+            setError(error.message);
+            console.log(error.message);
+          }
+        });
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
   };
 
   return (
     <div className="wrapper">
+      {error && <p style={{color: 'red'}}> {error}</p> }
       <div className="title">Simple Chatbot</div>
       <div ref={parentRef} className="box">
-
         {/* <div className="item">
           <div className="icon">
             <i className="fa fa-user"></i>
@@ -55,12 +78,16 @@ const ChatBot = () => {
             <p>Nice</p>
           </div>
         </div> */}
-        
       </div>
 
       <div className="typing-area">
         <div className="input-field">
-          <input type="text" placeholder="Type your message" required />
+          <input
+            ref={inputMsgRef}
+            type="text"
+            placeholder="Type your message"
+            required
+          />
           <button onClick={handleClick}>Send</button>
         </div>
       </div>
